0e6529fe15c2cd2727dc0fd7f2714bcd99c5fa35
javierglez2296 <81320903+javierglez2296@users.noreply.github.com>
2025-11-24 19:40:58 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+#!/bin/bash
+
+# Actualizar pip
+pip install --upgrade pip
+
+# Instalar dependencias
+pip install -r requirements.txt
+
+# Ejecutar FastAPI
+uvicorn main:app --host 0.0.0.0 --port $PORT
+=======
+#!/bin/bash
+
+# Actualizar pip
+pip install --upgrade pip
+
+# Instalar dependencias
+pip install -r requirements.txt
+
+# Ejecutar FastAPI
+uvicorn main:app --host 0.0.0.0 --port $PORT
+>>>>>>> 835ba31b483cfb9608de964e8d6a1d032d634d9a
